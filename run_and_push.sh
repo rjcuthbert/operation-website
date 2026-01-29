5085c88cb8e1eb63c1b7f9b0580bd7c5c8c86e8c
Ramiz <ramiz.cuthbert@gmail.com>
2026-01-29 21:35:06 +0000

    Auto update

--- run_and_push.sh
+++ run_and_push.sh
@@ -4,14 +4,6 @@
 set -e
 
 # Ensure argument is provided
-if [ -z "$1" ]; then
-  echo "Error: No argument provided."
-  echo "Usage: ./run_and_push.sh <page_name>"
-  exit 1
-fi
-
-
-
 
 echo "Pulling latest changes from origin..."
 git pull origin main
