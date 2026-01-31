23164211a05a1854cea99c8fda4accd44ce1b212
Ramiz <ramiz.cuthbert@gmail.com>
2026-01-31 17:01:40 +0000

    Auto update

--- run_and_push.sh
+++ run_and_push.sh
@@ -3,18 +3,19 @@
 # Exit on errors
 set -e
 
-# Ensure argument is provided
-
 cd ../backend
 
-python run_creator.py $1
-
+# Only run creator if an argument is provided
+if [ -n "$1" ]; then
+  python run_creator.py "$1"
+else
+  echo "No argument provided, skipping run_creator.py"
+fi
 
 cd ../frontend
 
 echo "Pulling latest changes from origin..."
 git pull origin main
-
 
 echo "Adding changes to git..."
 git add .
